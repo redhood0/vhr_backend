7db5aceda24ac0bda4666f8ed7c7e5c1abb21030
redhood <2043570808@qq.com>
2020-04-18 13:25:30 +0800

    #111

---
+++ src/main/java/com/redhood/vhr_end/VhrEndApplication.java
@@ -0,0 +1,13 @@
+package com.redhood.vhr_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhrEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VhrEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/redhood/vhr_end/VhrEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhood.vhr_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhrEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
